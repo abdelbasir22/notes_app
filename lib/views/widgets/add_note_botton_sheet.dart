ed104e1e1fd640bd69e69270b495d139dbcfcef6
abdelbasir22 <mohamedahmed1400@gmail.com>
2023-08-30 19:15:17 +0300

    finsh notes botton sheet

--- lib/views/widgets/add_note_botton_sheet.dart
+++ lib/views/widgets/add_note_botton_sheet.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/cutom_text_filed.dart';
+
+import 'custom_botton.dart';
 
 class AddNoteBottonSheet extends StatelessWidget {
   const AddNoteBottonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextFiled(
+              hint: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFiled(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 90,
+            ),
+            CustomBotton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
