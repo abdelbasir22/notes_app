bd26ed8bfdb6ad908724796662d75b28fee50dd8
abdelbasir22 <mohamedahmed1400@gmail.com>
2023-08-31 14:57:30 +0300

    edit

---
+++ lib/views/widgets/eidt_note_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EidtNotViewBody extends StatelessWidget {
+  const EidtNotViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
