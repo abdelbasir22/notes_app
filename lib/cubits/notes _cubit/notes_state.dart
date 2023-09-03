1bca375984867817fbac884c892ec5a5a3605fb6
abdelbasir22 <mohamedahmed1400@gmail.com>
2023-09-03 15:46:49 +0300

    ready to display notes

--- lib/cubits/notes _cubit/notes_state.dart
+++ lib/cubits/notes _cubit/notes_state.dart
@@ -4,17 +4,3 @@ part of 'notes_cubit.dart';
 sealed class NotesState {}
 
 final class NotesInitial extends NotesState {}
-
-final class NotesLoading extends NotesState {}
-
-final class Notessuccess extends NotesState {
-  final List<NoteModel> notes;
-
-  Notessuccess(this.notes);
-}
-
-final class NotesFailure extends NotesState {
-  final String errMessage;
-
-  NotesFailure(this.errMessage);
-}
